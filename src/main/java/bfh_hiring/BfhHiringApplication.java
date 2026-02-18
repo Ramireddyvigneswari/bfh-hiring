98f31fb9a040111126a02fa07e75906a35da48ca
Ramireddyvigneswari <vigneswariramireddy383@gmail.com>
2026-02-18 16:29:53 +0530

    added src folder

---
+++ src/main/java/bfh_hiring/BfhHiringApplication.java
@@ -0,0 +1,13 @@
+package bfh_hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfhHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfhHiringApplication.class, args);
+	}
+
+}
