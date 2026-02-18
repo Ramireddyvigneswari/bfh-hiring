8aff8ef22ab879cf0a02c466928c6f432d24d941
Ramireddyvigneswari <vigneswariramireddy383@gmail.com>
2026-02-18 17:25:51 +0530

    added src folder

---
+++ src/test/java/bfh_hiring/BfhHiringApplicationTests.java
@@ -0,0 +1,13 @@
+package bfh_hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfhHiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
